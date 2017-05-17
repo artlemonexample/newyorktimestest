496e809953fabc6435bad8075dab2739100d5641
aleksandr <ah_lemon_school@mail.ru>
2017-05-17 19:57:50 +0300

    Added project

---
+++ NYTimesTest/NYTimesTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NYTimesTest
+//
+//  Created by aleksandr on 17.05.17.
+//  Copyright © 2017 Sasha Haidar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NYTimesTest/NYTimesTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NYTimesTest
+//
+//  Created by aleksandr on 17.05.17.
+//  Copyright © 2017 Sasha Haidar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
